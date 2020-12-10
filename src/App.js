492c930c0dadeb64467be81e6dc2a6b2f8716030
Vlad Ageev <vageev06@gmail.com>
2020-12-10 19:41:02 +0200

    Created header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from 'components/Header';
+import store from 'app/store';
+import 'css/index.css';
 
 class App extends Component {
+  componentDidMount() {
+    window.addEventListener('resize', () => {
+      const windowSize = document.getElementsByTagName('html')[0].clientWidth;
+      store.dispatch({ type: 'windowSize/resize', payload: windowSize });
+    });
+  }
+
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path="/">
             <p>Root</p>
