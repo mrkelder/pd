14ffe1de3e0f7759bf5699317a6c6dcdf4b1bf71
Vlad Ageev <vageev06@gmail.com>
2020-12-14 14:14:56 +0200

    Created side bar

---
+++ src/components/SideBar.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useState } from 'react';
+import logo from 'img/logo.gif';
+import { Link, useLocation } from 'react-router-dom';
+import bo from 'img/burger_open.svg';
+import bc from 'img/burger_close.svg';
+import instagram from 'img/instagram.svg';
+import { useSelector } from 'react-redux';
+
+function SideBar() {
+  const { windowSize } = useSelector(state => state.windowSize);
+  const { pathname } = useLocation();
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  function openMenu() {
+    setMenuOpened(!menuOpened);
+  }
+
+  return (
+    <aside>
+      <Link to="/">
+        <img src={logo} alt="logo" id="logo" />
+      </Link>
+      { windowSize < 768 ?
+        <Fragment>
+          <button id="menu_button" onClick={openMenu} style={{ backgroundImage: `url('${menuOpened ? bc : bo}')` }} />
+          {menuOpened &&
+            <Fragment>
+              <Link to="/" style={{ fontWeight: `${pathname === '/' ? 'bold' : 'normal'}` }}>Home</Link>
+              <Link to="/shop" style={{ fontWeight: `${pathname === '/shop' ? 'bold' : 'normal'}` }}>Shop</Link>
+              <a target="_blank" rel="noreferrer" href="https://www.instagram.com/peaceful_disruption/?hl=en"><img src={instagram} alt="insta" id="insta" /></a>
+            </Fragment>
+          }
+        </Fragment>
+        :
+        <Fragment>
+          <Link to="/" style={{ fontWeight: `${pathname === '/' ? 'bold' : 'normal'}` }}>Home</Link>
+          <Link to="/shop" style={{ fontWeight: `${pathname === '/shop' ? 'bold' : 'normal'}` }}>Shop</Link>
+          <a target="_blank" rel="noreferrer" href="https://www.instagram.com/peaceful_disruption/?hl=en"><img src={instagram} alt="insta" id="insta" /></a>
+        </Fragment>
+      }
+
+    </aside>
+  )
+}
+
+export default SideBar
