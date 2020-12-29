6325063fc04ef8c8a300da78a430f6bbb34a7f5a
Vlad Ageev <vageev06@gmail.com>
2020-12-29 11:00:02 +0200

    Added 'big' and 'small' types of an item block

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 import ImgNotFound from 'img/imageNotFound.png';
 import 'css/item.css';
 
-function Item({ _id, name, price, img }) {
+function Item({ _id, name, price, img, type }) {
   const domain = useContext(infoContext);
 
   return (
-    <div className="item">
+    <div className={type === 'big' ? 'item' : 'item_s'}>
       <Link to={`/item/${_id}`}><img src={img !== 'none' ? `http://${domain}/static/${img}` : ImgNotFound} alt="item_photo" /></Link>
       <Link to={`/item/${_id}`}>{name}</Link>
       <p>${price}.00</p>
@@ -21,14 +21,16 @@ Item.defaultProps = {
   name: 'T-shirt',
   img: 'none',
   price: 999,
-  _id: 'none'
+  _id: 'none',
+  type: 'big'
 };
 
 Item.propTypes = {
   name: PropTypes.string,
   img: PropTypes.string,
   price: PropTypes.number,
-  _id: PropTypes.string
+  _id: PropTypes.string,
+  type: PropTypes.oneOf(['big', 'small'])
 };
 
 export default Item;
