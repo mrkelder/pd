a164e2ed2ea0db00a08dfa58aaf12a2fa779ee23
Vlad Ageev <vageev06@gmail.com>
2021-01-18 09:28:15 +0200

    Created shipping page for mobile

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,5 +1,5 @@
 import React, { Fragment, lazy, useState, Suspense, useEffect, useRef } from 'react';
-import { Breadcrumbs, Button, Typography, createStyles, FormControlLabel, TextField, createMuiTheme, ThemeProvider, Checkbox, Select, MenuItem, FormControl, InputLabel } from '@material-ui/core';
+import { Breadcrumbs, Button, Typography, RadioGroup, createStyles, Radio, FormControlLabel, TextField, createMuiTheme, ThemeProvider, Checkbox, Select, MenuItem, FormControl, InputLabel } from '@material-ui/core';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -20,6 +20,14 @@ const styles = createStyles({
   }
 });
 
+// const radioStyles = () => ({
+//   radio: {
+//     '&$checked': {
+//       color: '#197bbd'
+//     }
+//   }
+// });
+
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -37,12 +45,21 @@ function Payment() {
   const [submitStyle, setSubmitStyle] = useState({ height: '65px', fontSize: '1rem', textTransform: 'none', fontWeight: 'bold', fontFamily: 'Arial' });
   const [isSummaryOpened, setSummaryOpened] = useState(false);
   const [styleForBreadCrumbs, setStyleForBreadCrumbs] = useState(null);
-  // const [sectionHeight, setSectionHeight] = useState(null);
+  const [shipping, setShipping] = useState('fs');
+  const [stage, setStage] = useState(1); // stage of purchase
   const compared_h2 = useRef();
-  // const heading = useRef();
+  // const classes = radioStyles();
 
   function openSummary() {
     setSummaryOpened(!isSummaryOpened);
+  }
+
+  function stageChanger(stageNum) {
+    if (stage !== stageNum) setStage(stageNum);
+  }
+
+  function changeShipping({ target: { value } }) {
+    setShipping(value);
   }
 
   useEffect(() => {
@@ -55,15 +72,10 @@ function Payment() {
     if (windowSize >= 1000 && compared_h2.current !== undefined)
       setStyleForBreadCrumbs({ width: compared_h2.current.clientWidth });
     else setStyleForBreadCrumbs(null);
-    // Changes the height of the section
-    // if (windowSize >= 1000 && heading.current !== undefined)
-    //   setSectionHeight({ height: document.getElementsByTagName('html')[0].clientHeight - heading.current.clientHeight });
   }, [windowSize]);
 
   return (
     <div id="payment_page">
-      {/* { windowSize < 768 ?
-        <Fragment> */}
       <header id="payment_heading">
         <div id="link_block">
           <Link to="/">
@@ -81,8 +93,8 @@ function Payment() {
                 <img src={blue_arrow} alt="arrow" style={isSummaryOpened ? { transform: 'rotate(180deg)' } : { transform: 'rotate(0)' }} />
               </div>
               <div>
-                <span>$320.00</span>
-                <span>$310.00</span>
+                <span>{stage === 0 ? '$310.00' : shipping === 'fs' ? `$${(320 + 27.88).toFixed(2)}` : `$${(320 + 40.10).toFixed(2)}`}</span>
+                <span>{stage === 0 ? '$310.00' : shipping === 'fs' ? `$${(310 + 27.88).toFixed(2)}` : `$${(310 + 40.10).toFixed(2)}`}</span>
               </div>
             </div>
             <motion.div animate={isSummaryOpened ? { height: 'auto' } : { height: 0 }} transition={{ duration: .2 }} initial={false} id="summary_items" >
@@ -106,14 +118,14 @@ function Payment() {
               </div>
               <div className="info_block">
                 <div><p>Shipping</p></div>
-                <div><p>Calculated at next step</p></div>
+                <div><p>{stage === 0 ? 'Calculated at next step' : shipping === 'fs' ? '$27.88' : '$40.10'}</p></div>
               </div>
               <hr />
               <div className="info_block total">
                 <div><p>Total</p></div>
                 <div>
                   <p>CAD</p>
-                  <b>$310.00</b>
+                  <b>{stage === 0 ? '$310.00' : shipping === 'fs' ? `$${(310 + 27.88).toFixed(2)}` : `$${(310 + 40.10).toFixed(2)}`}</b>
                 </div>
               </div>
             </motion.div>
@@ -121,47 +133,93 @@ function Payment() {
         }
         <section id="input_sec">
           <Breadcrumbs id="bread_crumbs" style={styleForBreadCrumbs} separator={<img src={arrow} alt="arrow" style={{ transform: 'rotate(180deg)', width: '8px' }} />} aria-label="breadcrumb">
-            <Link to="/" >Cart</Link>
-            <p>Information</p>
-            <p>Shipping</p>
+            <Link to="/cart" >Cart</Link>
+            <p style={stage === 0 ? { fontWeight: 'bold' } : { color: '#197bbd', cursor: 'pointer' }} onClick={() => { stageChanger(0); }}>Information</p>
+            <p style={stage === 1 ? { fontWeight: 'bold' } : null}>Shipping</p>
           </Breadcrumbs>
           <ThemeProvider theme={theme}>
-            <form>
-              <h2 ref={compared_h2}>Contact Information</h2>
-              <Input label="Email or mobile phone number" />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="checkedB"
-                    color="primary"
-                  />
-                }
-                label={<Typography style={styles.formControlLabel}>Keep me up to date on news and exclusive offers</Typography>}
-              />
-              <h2>Shipping address</h2>
-              <div className="inline_inputs">
-                <Input label="First name" name="checkout[shipping_address][first_name]" />
-                <div className="gap" />
-                <Input label="Last name" name="checkout[shipping_address][last_name]" />
+            {stage === 0 &&
+              <form>
+                <h2 ref={compared_h2}>Contact Information</h2>
+                <Input label="Email or mobile phone number" />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name="checkedB"
+                      color="primary"
+                    />
+                  }
+                  label={<Typography style={styles.formControlLabel}>Keep me up to date on news and exclusive offers</Typography>}
+                />
+                <h2>Shipping address</h2>
+                <div className="inline_inputs">
+                  <Input label="First name" name="checkout[shipping_address][first_name]" />
+                  <div className="gap" />
+                  <Input label="Last name" name="checkout[shipping_address][last_name]" />
+                </div>
+                <Input label="Address" name="checkout[shipping_address][address1]" />
+                <Input label="Apartment, suite, etc. (optional)" name="checkout[shipping_address][address2]" />
+                <Input label="City" name="checkout[shipping_address][city]" />
+                <div className="inline_inputs">
+                  <FormControl variant="outlined" className="c_input" size="small">
+                    <InputLabel id="demo-simple-select-outlined-label">Country</InputLabel>
+                    <Select labelId="demo-simple-select-outlined-label" value="none" label="Country">
+                      <MenuItem value="none">---</MenuItem>
+                      <MenuItem value="usa">the USA</MenuItem>
+                      <MenuItem value="canada">Canada</MenuItem>
+                      <MenuItem value="ukraine">Ukraine</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <div className="gap" />
+                  <Input label="Postal code" />
+                </div>
+                <Button type="submit" onClick={e => { e.preventDefault(); }} className="c_input submit_btn" variant="contained" size="medium" color="primary" style={submitStyle}>Continue to shipping</Button>
+              </form>
+            }
+            {stage === 1 &&
+              <div id="second_stage">
+                <div id="change_info">
+                  <div className="change_info_option">
+                    <div className="change_info_heading">
+                      <span>Contact</span>
+                      <span onClick={() => { stageChanger(0); }}>Change</span>
+                    </div>
+                    <address>someEmail@gmail.com</address>
+                  </div>
+                  <hr />
+                  <div className="change_info_option">
+                    <div className="change_info_heading">
+                      <span>Ship to</span>
+                      <span onClick={() => { stageChanger(0) }}>Change</span>
+                    </div>
+                    <address>Ulitsa shorca, Киев, 472758, Ukraine</address>
+                  </div>
+                </div>
+                <h2>Shipping to</h2>
+                <RadioGroup name="gender1" value={shipping} onChange={changeShipping}>
+                  <div id="choose_shipping">
+                    <div className="change_info_option">
+                      <Radio value="fs" color="primary" name="shipping" />
+                      <div className="change_shipping_info">
+                        <span className="shipping_name">Canada Post Small Packet International Surface</span>
+                        <span className="shipping_time">28 to 84 business days</span>
+                      </div>
+                      <span className="price">$27.88</span>
+                    </div>
+                    <hr />
+                    <div className="change_info_option">
+                      <Radio value="ss" color="primary" name="shipping" />
+                      <div className="change_shipping_info">
+                        <span className="shipping_name">Canada Post Small Packet International Air</span>
+                        <span className="shipping_time">6 to 12 business days</span>
+                      </div>
+                      <span className="price">$40.10</span>
+                    </div>
+                  </div>
+                </RadioGroup>
+                <Button type="submit" onClick={e => { e.preventDefault(); }} className="c_input submit_btn" variant="contained" size="medium" color="primary" style={submitStyle}>Continue to shipping</Button>
               </div>
-              <Input label="Address" name="checkout[shipping_address][address1]" />
-              <Input label="Apartment, suite, etc. (optional)" name="checkout[shipping_address][address2]" />
-              <Input label="City" name="checkout[shipping_address][city]" />
-              <div className="inline_inputs">
-                <FormControl variant="outlined" className="c_input" size="small">
-                  <InputLabel id="demo-simple-select-outlined-label">Country</InputLabel>
-                  <Select labelId="demo-simple-select-outlined-label" value="none" label="Country">
-                    <MenuItem value="none">---</MenuItem>
-                    <MenuItem value="usa">the USA</MenuItem>
-                    <MenuItem value="canada">Canada</MenuItem>
-                    <MenuItem value="ukraine">Ukraine</MenuItem>
-                  </Select>
-                </FormControl>
-                <div className="gap" />
-                <Input label="Postal code" />
-              </div>
-              <Button type="submit" className="c_input submit_btn" variant="contained" size="medium" color="primary" style={submitStyle}>Continue to shipping</Button>
-            </form>
+            }
           </ThemeProvider>
           {windowSize >= 1000 &&
             <footer>
@@ -199,7 +257,7 @@ function Payment() {
               <div><p>Total</p></div>
               <div>
                 <p>CAD</p>
-                <b>$310.00</b>
+                <b>{stage === 0 ? '$310.00' : shipping === 'fs' ? `$${(310 + 27.88).toFixed(2)}` : `$${(310 + 40.10).toFixed(2)}`}</b>
               </div>
             </div>
           </section>
@@ -211,11 +269,6 @@ function Payment() {
           <p>All rights reserved Peaceful Disruption</p>
         </footer>
       }
-      {/* </Fragment>
-        :
-        <Fragment>
-        </Fragment>
-      } */}
     </div>
   );
 }
