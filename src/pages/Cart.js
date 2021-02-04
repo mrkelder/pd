15986d57a5863bb0a81660b6beb4f168f5a6658b
Vlad Ageev <vageev06@gmail.com>
2021-02-04 13:11:54 +0200

    Added "details" for PC version

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -17,22 +17,24 @@ function Cart() {
         <p>Your Shopping Cart</p>
       </Breadcrumbs>
       { windowSize >= 1000 &&
-        <div id="cart_items_options">
+        <section id="cart_items_options">
           <span>Price</span>
           <span>Quantity</span>
           <span>Total</span>
-        </div>
+        </section>
       }
-      <div id="cart_items">
+      <section id="cart_items">
         <CartItem />
         {
-          new Array(15).fill(1).map((i, index) => <CartItem img="a_hoodie.webp" price={5} key={`cart_${index}`} />)
+          new Array(1).fill(1).map((i, index) => <CartItem img="a_hoodie.webp" price={5} key={`cart_${index}`} />)
         }
-      </div>
-      <div id="details">
-        <h2>Special instructions for seller</h2>
-        <textarea value={details} onChange={({ target: { value } }) => { setDetails(value); }} />
-      </div>
+      </section>
+      <section id="payment">
+        <div id="details">
+          <h2>Special instructions for seller</h2>
+          <textarea value={details} onChange={({ target: { value } }) => { setDetails(value); }} />
+        </div>
+      </section>
     </div>
   );
 }
