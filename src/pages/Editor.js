39039cf61574f80a8c550deb879f454d55ebbf94
Vlad Ageev <vageev06@gmail.com>
2021-02-15 20:06:43 +0200

    Created a price system for a custom item

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
+import React, { useContext, useEffect, useState, useRef, useCallback } from 'react';
 import { fabric } from 'fabric';
 import arrow from 'img/blue_arrow.svg';
 import { infoContext } from 'app/context';
@@ -14,20 +14,65 @@ import { useSelector } from 'react-redux';
 function Editor() {
   const domain = useContext(infoContext);
   const images = ['a_hoodie.webp', 'a_hoodie_back.webp'];
+  const basePrice = 130;
   const [imageIndex, setImageIndex] = useState(0);
   const [canvas, setCanvas] = useState(null);
   const [color, setColor] = useState('#333');
   const [colorOpen, setOpenColor] = useState(false);
   const [elements, setElements] = useState([[], []]);
   const [removedElements, setRemovedElements] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(basePrice);
   const fileElement = useRef();
   const { windowSize } = useSelector(state => state.windowSize);
+
+  const getTotalPrice = useCallback(() => {
+    // Gets size of each element and calculates a total price
+    function getTotalPriceOnCertainImage(i) {
+      const { class: className, scaleX, scaleY } = i;
+      if (className !== "deleted") {
+        if (className === "txt") {
+          // If we're working with some text
+          const { text } = i;
+          const priceIndex = 0.25;
+          const square = scaleX * scaleY;
+          const letters = text.split(/ +/).join('').length;
+          totalTxtPrice += square * priceIndex * letters;
+        }
+        else {
+          // If we're working with an image
+          const { width, height } = i;
+          const priceIndex = 100000;
+          const scale = scaleX * scaleY * 100; // multiplied by 100 because default scale if 0.1
+          const square = width * height;
+          totalImgPrice += square * scale / priceIndex;
+          console.log(totalImgPrice)
+        }
+      }
+    }
+    let totalImgPrice = 0;
+    let totalTxtPrice = 0;
+    for (const i of elements[0]) {
+      getTotalPriceOnCertainImage(i);
+    }
+    for (const i of elements[1]) {
+      getTotalPriceOnCertainImage(i);
+    }
+    setTotalPrice(totalImgPrice + totalTxtPrice + basePrice);
+  }, [elements]);
+
+  useEffect(() => {
+    getTotalPrice();
+  }, [elements, getTotalPrice]);
 
   useEffect(() => {
     const canvasInst = new fabric.Canvas('canvas_editor');
     canvasInst.selection = false;
     setCanvas(canvasInst);
   }, []);
+
+  useEffect(() => {
+    if (canvas) canvas.on('object:modified', getTotalPrice)
+  }, [getTotalPrice, canvas]);
 
   function addItemToBin() {
     alert("Hey, you've created your own style 🤑. Congrats! But we don't have this system yet, sorry 😥");
@@ -40,11 +85,13 @@ function Editor() {
 
   function addText() {
     const textbox = new fabric.Textbox('New Text', {
-      left: 15,
-      top: 15,
+      left: 32,
+      top: 35,
       width: 80,
       fontFamily: 'Bold',
-      fill: '#333'
+      fill: '#333',
+      class: "txt",
+      opacity: 1
     });
     textbox.setControlsVisibility({ mt: false, mb: false });
     canvas.add(textbox).setActiveObject(textbox);
@@ -59,7 +106,7 @@ function Editor() {
     reader.onload = f => {
       const data = f.target.result;
       fabric.Image.fromURL(data, img => {
-        const oImg = img.set({ left: 0, top: 0, angle: 0 }).scale(0.1);
+        const oImg = img.set({ left: 0, top: 0, angle: 0, class: "img", opacity: 1 }).scale(0.1);
         oImg.setControlsVisibility({ mt: false, mb: false, ml: false, mr: false });
         addToElementsCollection(oImg);
         canvas.add(oImg).renderAll();
@@ -122,6 +169,7 @@ function Editor() {
       element.set({ selectable: false });
       element.opacity = 0;
       element.hoverCursor = 'default';
+      element.set({ class: 'deleted' });
       canvas.discardActiveObject().renderAll();
       setRemovedElements([...removedElements, element]);
     }
@@ -129,7 +177,7 @@ function Editor() {
   }
 
   return (
-    <div id="editor_page">
+    <section id="editor_page">
       <div id="interface">
         {windowSize >= 1000 &&
           <div id="tool_bar">
@@ -185,8 +233,12 @@ function Editor() {
           </label>
         </div>
       }
-      <Button click={addItemToBin}>ADD TO CART</Button>
-    </div >
+      <section id="editor_footer">
+        <p className="price">Base price: ${basePrice.toFixed(2)}</p>
+        <p className="price">Total price: ${totalPrice.toFixed(2)}</p>
+        <Button click={addItemToBin}>ADD TO CART</Button>
+      </section>
+    </section>
   );
 }
 
